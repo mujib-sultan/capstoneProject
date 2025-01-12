17a3854ec71fb23c617addc84f27f8032e45bd09
fetehadin <fetehadinnegash@gmail.com>
2025-01-12 17:55:05 +0300

    initial commit

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
