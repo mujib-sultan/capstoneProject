ab46458b205b048fd1238ed4796b252f8a700875
fetehadin <fetehadinnegash@gmail.com>
2025-01-13 12:01:34 +0300

    update project

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import dj_database_url
+#import dj_database_url
 from pathlib import Path
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,18 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
+DEBUG = True
 
-allowed_hosts = os.environ.get('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = []
+
+"""allowed_hosts = os.environ.get('ALLOWED_HOSTS', '')
 if isinstance(allowed_hosts, str):
     ALLOWED_HOSTS = allowed_hosts.split(' ')
 elif isinstance(allowed_hosts, list):
     ALLOWED_HOSTS = allowed_hosts
 else:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []"""
 
 
 # Application definition
@@ -90,9 +93,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',  # This will create a file in your project directory
     }
 }
-database_url = os.environ.get('DATABASE_URL')
+"""database_url = os.environ.get('DATABASE_URL')
 if database_url:
     DATABASES['default'] = dj_database_url.parse(database_url)
+elif not DEBUG:
+    raise ValueError("DATABASE_URL environment variable not set in production environment")
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
